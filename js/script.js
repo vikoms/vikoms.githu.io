89f5215124f568152fccddf439bee9934d7a097d
Viko Muhammad Saputra <vikoms95@gmail.com>
2020-05-20 23:06:16 +0700

    modify portfolio section

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ $(window).scroll(function () {
 
   // Portfolio animation
 
-  if (wScroll > $('.portfolio').offset().top - 100) {
+  if (wScroll > $('.portfolio').offset().top - 150) {
 
     $('.box-portfolio').each(function (index) {
       setTimeout(function () {
@@ -63,16 +63,16 @@ const contentPortfolio = [{
     content_left: `<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
                 <div class="carousel-inner">
                   <div class="carousel-item active">
-                    <img src="project/hefastore_1.png" class="d-block w-100" alt="...">
+                    <img src="project/hefastore_1.jpg" class="d-block" alt="...">
                   </div>
                   <div class="carousel-item">
-                    <img src="project/hefastore_2.png" class="d-block w-100" alt="...">
+                    <img src="project/hefastore_2.jpg" class="d-block" alt="...">
                   </div>
                   <div class="carousel-item">
-                    <img src="project/hefastore_3.png" class="d-block w-100" alt="...">
+                    <img src="project/hefastore_3.jpg" class="d-block" alt="...">
                   </div>
                   <div class="carousel-item">
-                    <img src="project/hefastore_4.png" class="d-block w-100" alt="...">
+                    <img src="project/hefastore_4.jpg" class="d-block" alt="...">
                   </div>
                 </div>
                 <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
@@ -84,23 +84,130 @@ const contentPortfolio = [{
                   <span class="sr-only">Next</span>
                 </a>
               </div>`,
-    content_right: `<h3 class = "text-center mt-3">HefaStore</h3>`
+    content_right: `<h2 class="text-center">HefaStore</h2>
+              <li class="title">
+                Apa itu Hefastore?
+              </li>
+              <p class="value-1">Hefa Store adalah tampilan halaman website untuk Toko Online (E-Commerce)</p>
+              <li class="title">
+                Halaman
+                <ul class="list">
+                  <li>Halaman Home</li>
+                  <li>Halaman Detail Produk</li>
+                  <li>Halaman Kategori</li>
+                  <li>Halaman Checkout</li>
+                </ul>
+              </li>
+
+              <li class="title">
+                Dibuat Menggunakan :
+                <ul class="list">
+                  <li>HTML5</li>
+                  <li>CSS3</li>
+                  <li>Bootstrap 4</li>
+                  <li>Javascript</li>
+                </ul>
+              </li>`
   },
   {
-    content_left: `<iframe width="560" height="500" src="https://www.youtube.com/embed/2nCbYWFZtxY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+    content_left: `<iframe width="560" height="315" src="https://www.youtube.com/embed/2nCbYWFZtxY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
     content_right: `
-    <h3 class = "text-center mt-3">Jaga Tubuh App</h3>
+     <h2 class="text-center">Jaga Tubuh</h2>
+              <li class="title">
+                Apa itu Aplikasi Jaga Tubuh?
+              </li>
+              <p class="value-1">Jaga Tubuh adalah aplikasi kebugaran dan kesehatan yang dapat membantu pengguna untuk
+                manjalani hidup sehat dan juga untuk menurunkan berat badan </p>
+              <li class="title">
+                Fitur
+                <ul class="list">
+                  <li>Program Workout 30 Hari</li>
+                  <li>Jadwal Diet 30 Hari</li>
+                  <li>Laporan Berat Badan</li>
+                </ul>
+              </li>
+
+              <li class="title">
+                Dibuat Menggunakan :
+                <ul class="list">
+                  <li>Java</li>
+                  <li>Laravel</li>
+                  <li>XML</li>
+                  <li>Shared Preferences</li>
+                  <li>SQlite</li>
+                  <li>MYSQL</li>
+                </ul>
+              </li>
    `
   },
   {
-    content_left: `<iframe width="560" height="500" src="https://www.youtube.com/embed/2nCbYWFZtxY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+    content_left: `<iframe width="560" height="315" src="https://www.youtube.com/embed/_fXZvrEkTpw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
     content_right: ` 
-   <h3 class = "text-center mt-3">Edupay Application</h3><a href = "https://github.com/vikoms/EduPay-AplikasiPembayaranSPP"
-    class = "btn btn-success mt-5"
-    target = "_blank" > See On
-    Github </a>
+              <h2 class="text-center">EduPay</h2>
+              <li class="title">
+                Apa itu EduPay?
+              </li>
+              <p class="value-1">Edupay adalah sebuah aplikasi untuk mengelola pembayaran sekolah </p>
+              <li class="title">
+                Fitur
+                <ul class="list">
+                  <li>Kelola data Siswa,Petugas,Kelas,SPP (admin)</li>
+                  <li>Kelola Riwayat pembayaran siswa (admin & petugas)</li>
+                  <li>Entry Data Pembayaran (admin & petugas)</li>
+                  <li>Melihat daftar riwayat pembayaran untuk siswa </li>
+                </ul>
+              </li>
+
+              <li class="title">
+                Dibuat Menggunakan :
+                <ul class="list">
+                  <li>Java</li>
+                  <li>XML</li>
+                  <li>Firebase</li>
+                  <li>Shared Preferences</li>
+                  <li>MVVM Design Patern</li>
+                </ul>
+              </li>
+              <a href="https://github.com/vikoms/EduPay-AplikasiPembayaranSPP" target="_black"
+                class="btn btn-primary">Lihat di Github</a>
   `
+  },
+  {
+    content_left: `<iframe src="https://www.youtube.com/embed/FzSzF0IhLu8" frameborder="0"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen></iframe>`,
+    content_right: `<h2 class="text-center">Moflix</h2>
+              <li class="title">
+                Apa itu Moflix?
+              </li>
+              <p class="value-1">Moflix adalah sebuah aplikasi yang berguna untuk menampilkan daftar film , serial tv,
+                maupun
+                artis </p>
+              <li class="title">
+                Fitur
+                <ul class="list">
+                  <li>Daftar Film , Serial TV dan Artis</li>
+                  <li>Cari Film, Serial TV dan Artis</li>
+                  <li>Menambahkan Film dan Serial TV favorit</li>
+                  <li>Notifikasi pengigat untuk masuk ke aplikasi setiap jam 07:00 </li>
+                  <li>Notifikasi untuk menampilkan daftar film yang tayang pada hari ini setiap jam 08:00 </li>
+                  <li>Multi Bahasa (Bahasa Inggris dan Bahasa Indonesia)</li>
+                </ul>
+              </li>
+
+              <li class="title">
+                Dibuat Menggunakan :
+                <ul class="list">
+                  <li>Kotlin</li>
+                  <li>SQLite (untuk menyimpan daftar film dan serial tv favorit)</li>
+                  <li>Alarm Manager</li>
+                  <li>API : <a href="https://www.themoviedb.org" target="_blank"> The MovieDB</a></li>
+                  <li>MVVM Design Pattern </li>
+                  <li>XML</li>
+                </ul>
+              </li>`
   }
+
 ]
 
 const togglePortfolio = document.querySelectorAll('.toggle-portfolio')
